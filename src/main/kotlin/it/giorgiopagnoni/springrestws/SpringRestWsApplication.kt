71f6c548f7d0611596fbeef068808000aa5535a3
Giorgio Pagnoni <giorgio.pagnoni@sintattica.it>
2018-08-14 17:54:30 +0800

    first commit

---
+++ src/main/kotlin/it/giorgiopagnoni/springrestws/SpringRestWsApplication.kt
@@ -0,0 +1,11 @@
+package it.giorgiopagnoni.springrestws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRestWsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringRestWsApplication>(*args)
+}
---
+++ src/test/kotlin/it/giorgiopagnoni/springrestws/SpringRestWsApplicationTests.kt
@@ -0,0 +1,16 @@
+package it.giorgiopagnoni.springrestws
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringRestWsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
