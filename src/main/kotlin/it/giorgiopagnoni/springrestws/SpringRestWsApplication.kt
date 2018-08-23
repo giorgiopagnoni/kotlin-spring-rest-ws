5e883b5b43bcb897fa5644fc724231f2999b78d7
Giorgio Pagnoni <giorgio.pagnoni@sintattica.it>
2018-08-23 17:03:31 +0800

    packaging as war

--- src/main/kotlin/it/giorgiopagnoni/springrestws/SpringRestWsApplication.kt
+++ src/main/kotlin/it/giorgiopagnoni/springrestws/SpringRestWsApplication.kt
@@ -2,12 +2,20 @@ package it.giorgiopagnoni.springrestws
 
 import it.giorgiopagnoni.springrestws.security.AppProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @SpringBootApplication
-class SpringRestWsApplication {
+class SpringRestWsApplication : SpringBootServletInitializer() {
+
+    // to be run
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(SpringRestWsApplication::class.java)
+    }
+
     @Bean
     fun bCryptPasswordEncoder() = BCryptPasswordEncoder()
 
